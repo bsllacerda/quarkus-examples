f15fb9784d4046f6b2590addcf797d43464ed559
bsl.lacerda <bsl.lacerda@gmail.com>
2023-07-25 08:43:31 -0300

    Subida inicial

---
+++ quarkus-mysql-connection/src/main/java/com/example/DatabaseResource.java
@@ -0,0 +1,64 @@
+package com.example;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.jboss.logging.Logger;
+
+@Path("/db")
+public class DatabaseResource {
+
+    private static final Logger LOG = Logger.getLogger(DatabaseResource.class);
+
+    @Inject
+    DataSource dataSource;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public ConnectionTestResult testConnection() {
+        ConnectionTestResult result = new ConnectionTestResult();
+        try (Connection connection = dataSource.getConnection()) {
+            if (connection.isValid(1000)) {
+                result.setStatus("SUCCESS");
+                result.setMessage("Connection is valid! 🙂");
+                LOG.info("Connection is valid!");
+            } else {
+                result.setStatus("FAILURE");
+                result.setMessage("Connection is not valid. 😢");
+                LOG.warn("Connection is not valid.");
+            }
+        } catch (SQLException exception) {
+            result.setStatus("ERROR");
+            result.setMessage("Failed to create a connection. 😢");
+            LOG.error("Failed to create a connection.", exception);
+        }
+        return result;
+    }
+
+    public static class ConnectionTestResult {
+        private String status;
+        private String message;
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+    }
+}
