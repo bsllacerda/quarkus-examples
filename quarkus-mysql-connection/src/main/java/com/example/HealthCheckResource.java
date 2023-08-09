b5699e67947976919951d5bf5e97df5775fd0b39
bsl.lacerda <bsl.lacerda@gmail.com>
2023-08-09 05:24:54 -0300

    Update

--- quarkus-mysql-connection/src/main/java/com/example/HealthCheckResource.java
+++ quarkus-mysql-connection/src/main/java/com/example/HealthCheckResource.java
@@ -5,27 +5,65 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-import org.jboss.logging.Logger;
 import io.quarkus.logging.Log;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Path("")
 public class HealthCheckResource {    
 
     @Inject
     DataSource dataSource;
+	
+	private final LocalDateTime startTime = LocalDateTime.now();
+	
+	/**
+	 * Neste código, ao instanciar StartupProbeResource, a hora atual é registrada.
+	 * Cada vez que o endpoint /startup é acessado, calculamos quantos segundos se passaram desde a inicialização.
+	 * Durante os primeiros 60 segundos, o endpoint retorna um status HTTP 503 (Service Unavailable).
+	 * Após 60 segundos, ele retorna um status HTTP 200 com a mensagem "Service ready!".
+	 */
+	@GET
+    @Produces(MediaType.TEXT_PLAIN)
+	@Path("/startup")
+    public Response startupCheck() {		
+
+        Log.info("Executando startupCheck");
+		
+		LocalDateTime now = LocalDateTime.now();
+        long secondsSinceStart = ChronoUnit.SECONDS.between(startTime, now);
+
+        if (secondsSinceStart < 60) {
+			Log.warn("Not Started Yet! A Aplicação ainda está inicializando...");
+            // Return a 503 Service Unavailable status for the first 60 seconds
+            return Response.status(Response.Status.SERVICE_UNAVAILABLE)
+                    .entity("Application is still warming up!")
+                    .build();
+        }
+		
+		Log.info("Startup OK! Aplicação inicializada!");
+		
+        return Response.ok("Application started!").build();
+
+    }
 
 	@GET
     @Produces(MediaType.APPLICATION_JSON)
 	@Path("/healthz")
-    public ConnectionTestResult healthzCheck() {        
+    public ConnectionTestResult healthzCheck() {   
+	
 		Log.info("Executando healthCheck");
+
 		ConnectionTestResult result = new ConnectionTestResult();        
         result.setStatus("SUCCESS");
         result.setMessage("OK");
+
         Log.info("Health: OK");            
+
         return result;
     }
 
@@ -33,25 +71,37 @@ public class HealthCheckResource {
     @Produces(MediaType.APPLICATION_JSON)
 	@Path("/ready")
     public ConnectionTestResult readyCheck() {
+
         Log.info("Executando readyCheck");
+
 		ConnectionTestResult result = new ConnectionTestResult();
+
         try (Connection connection = dataSource.getConnection()) {
+			
             if (connection.isValid(1000)) {
+				
                 result.setStatus("SUCCESS");
                 result.setMessage("OK");
+				
                 Log.info("Ready: OK");
+				
             } else {
+				
                 result.setStatus("FAILURE");
                 result.setMessage("Failed");
-                Log.warn("Failed. Connection is not valid.");
+				
+                Log.warn("Not Ready! Connection is not valid.");
             }
         } catch (SQLException exception) {
+			
             result.setStatus("ERROR");
             result.setMessage("Failed to create a connection. \nMessage: " + exception.getMessage());
-            Log.error("Failed to create a connection.", exception);
+			
+            Log.error("Not Ready! Failed to create a connection.", exception);
         }
+
         return result;
-    }
+    }	
 
     public static class ConnectionTestResult {
         private String status;
